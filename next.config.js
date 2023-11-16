e7620748c314bf0d56f516e84a3a808bd8b371e5
Maxson Araújo <maxson.jordan@gmail.com>
2023-11-16 18:43:30 -0300

    feito

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const { argv } = require("process");
 
 if (!(process.argv && process.argv.includes("build"))) {
 
-  if(process.env.DEBUG_ENV){
-    console.log("ENV ATUAL:",process.env)
+  if (process.env.DEBUG_ENV) {
+    console.log("ENV ATUAL:", process.env)
   }
 
   if (!process.env.SOCKETURL) {
@@ -20,7 +20,7 @@ if (!(process.argv && process.argv.includes("build"))) {
     console.log("listening socket on http://" + urlSock)
   } else {
     const port = process.env.CUSTOM_PORT_SEC ?? 3002//Math.floor((new Date().getTime() % 10000) + 1000);
-    const host = process.env.CUSTOM_HOST ?? process.env.HOSTNAME ?? '127.0.0.1'
+    const host = process.env.CUSTOM_HOST ?? '127.0.0.1'
     const urlSock = host + ":" + port;
     process.env.SOCKETURL = urlSock;
 
