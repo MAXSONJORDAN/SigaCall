24704f4fceb3a96ae6c18407c4fb331fa176387e
Maxson Araújo <maxson.jordan@gmail.com>
2023-11-16 16:51:27 -0300

    change to host

--- next.config.js
+++ next.config.js
@@ -2,26 +2,31 @@ const withPlugins = require("next-compose-plugins");
 /** @type {import('next').NextConfig} */
 const withFonts = require("next-fonts");
 const app = require('./src/integrations/socket.io/index.js');
+const { argv } = require("process");
 
-if (!process.env.SOCKETURL) {
-  const port = 3001//Math.floor((new Date().getTime() % 10000) + 1000);
-  const urlSock = "127.0.0.1:" + port;
-  process.env.SOCKETURL = urlSock;
+if (!(process.argv && process.argv.includes("build"))) {
 
-  app.listen(port)
-  console.log("listening socket on http://" + urlSock)
-}else{
-  const port = 3002//Math.floor((new Date().getTime() % 10000) + 1000);
-  const urlSock = "127.0.0.1:" + port;
-  process.env.SOCKETURL = urlSock;
+  if (!process.env.SOCKETURL) {
+    const port = process.env.CUSTOM_PORT ?? 3001//Math.floor((new Date().getTime() % 10000) + 1000);
+    const host = process.env.CUSTOM_HOST ?? process.env.HOST ?? '127.0.0.1'
+    const urlSock = host + ":" + port;
+    process.env.SOCKETURL = urlSock;
 
-  app.listen(port)
-  console.log("listening socket on http://" + urlSock)
+    app.listen(port)
+    console.log("listening socket on http://" + urlSock)
+  } else {
+    const port = process.env.CUSTOM_PORT_SEC ?? 3002//Math.floor((new Date().getTime() % 10000) + 1000);
+    const host = process.env.CUSTOM_HOST ?? process.env.HOST ?? '127.0.0.1'
+    const urlSock = host + ":" + port;
+    process.env.SOCKETURL = urlSock;
+
+    app.listen(port)
+    console.log("listening socket on http://" + urlSock)
+  }
 }
 
-
 const nextConfig = {
-  experimental:{
+  experimental: {
     serverActions: true,
   }
   // async rewrites() {
