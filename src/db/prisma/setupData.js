07540d9b615b33aadf59dcc4f2d37fddb615a9e5
Maxson Araújo <maxson.jordan@gmail.com>
2023-11-17 15:48:38 -0300

    feito

--- src/db/prisma/setupData.js
+++ src/db/prisma/setupData.js
@@ -18,12 +18,63 @@ console.log("SETUP DATA");
         }
     ]
 
-    
+    const tratamentos = [
+        {
+            "id": 1,
+            "identificador": "Dr.",
+            "pronuncia": "Doutor"
+        },
+        {
+            "id": 2,
+            "identificador": "Dra.",
+            "pronuncia": "Doutora"
+        },
+        {
+            "id": 3,
+            "identificador": "Sr.",
+            "pronuncia": "Senhor"
+        },
+        {
+            "id": 4,
+            "identificador": "Sra.",
+            "pronuncia": "Senhora"
+        },
+        {
+            "id": 5,
+            "identificador": "Enfermeiro",
+            "pronuncia": "Enfermeiro"
+        },
+        {
+            "id": 6,
+            "identificador": "Enfermeira",
+            "pronuncia": "Enfermeira"
+        },
+        {
+            "id": 7,
+            "identificador": "O",
+            "pronuncia": "O"
+        },
+        {
+            "id": 9,
+            "identificador": "A",
+            "pronuncia": "A"
+        }
+    ]
+
+
     console.log("Roles...");
     for (let i = 0; i < roles.length; i++) {
         const role = roles[i];
         await db.role.upsert({
             create: role, update: role, where: { id: role.id },
+        })
+    }
+
+    console.log("Tratamentos...");
+    for (let i = 0; i < tratamentos.length; i++) {
+        const tratamento = tratamentos[i];
+        await db.role.upsert({
+            create: tratamento, update: tratamento, where: { id: tratamento.id },
         })
     }
 
